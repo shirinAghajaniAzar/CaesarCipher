b7e90eaebdd612865f387778b6cb87eddb8ce3a3
SHIRIN <ss.shirin.hh@gmail.com>
2026-06-14 17:16:59 +0330

    combine encrypt and decrypt function and make caesar()

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -6,32 +6,16 @@ shift= int(input("Type the shift number:\n"))
 
 shift = shift % len(alphabet)
 
-def encrypt(original_text, shift_amount):
-    place_holder=''
-    for letter in original_text:
+def caeser(original_text, shift, direction):
+      place_holder=''
+      if direction=='decode':
+        shift *= -1
+      for letter in original_text:
         if letter in alphabet:
             x = (alphabet.index(letter) + shift) % len(alphabet)
             place_holder += alphabet[x]
         else:
             place_holder += letter
-    print(f"The cipher message is:\n {place_holder}")
+      print(f"The message is:\n {place_holder}")
 
-
-def decrypt(cipher_text, shift_amount):
-    place_holder= ''
-    ceaser_text = text
-    shift_amount = shift
-
-    for letter in text:
-        if letter in alphabet:
-            x = (alphabet.index(letter) - shift) % len(alphabet)
-            place_holder += alphabet[x]
-        else:
-            place_holder += letter
-    print(f"The original message was:\n {place_holder}")        
-
-if direction=='encode':
-    encrypt(original_text= text ,shift_amount = shift)
-
-elif direction=='decode':
-    decrypt(cipher_text = text, shift_amount= shift)
+caeser(original_text=text, shift= shift, direction= direction)
