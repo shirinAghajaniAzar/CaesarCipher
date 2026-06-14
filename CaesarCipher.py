b98b42efceb6cef13c872c75be06cfcaa1c35bae
SHIRIN <ss.shirin.hh@gmail.com>
2026-06-14 14:44:03 +0330

    changing 26 to len(alphabet) 2

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -4,7 +4,7 @@ direction= input("Type 'encode' to encrypt. type 'decode' to decrypt:\n").lower(
 text= input("Type your message:\n").lower()
 shift= int(input("Type the shift number:\n"))
 
-shift = shift % 26
+shift = shift % len(alphabet)
 
 def encrypt(original_text, shift_amount):
     place_holder=''
