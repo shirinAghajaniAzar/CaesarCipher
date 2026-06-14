43b41ad424aabbd08dbe612fbe78cfee749aa3bb
SHIRIN <ss.shirin.hh@gmail.com>
2026-06-14 22:32:12 +0330

    adding ascii art

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,5 +1,9 @@
+import asciiart
+
 alphabet= ['a', 'b', 'c' ,'d', 'e','f','g','h','i','j', 'k','l','m','n','o', 'p', 'q','r','s','t', 'u', 'v', 'w','x','y','z']
 
+
+print(asciiart.logo)
 direction= input("Type 'encode' to encrypt. type 'decode' to decrypt:\n").lower()
 text= input("Type your message:\n").lower()
 shift= int(input("Type the shift number:\n"))
